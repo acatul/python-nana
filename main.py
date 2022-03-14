680d7736b8a99eb025379ca1d069f628964a1c48
acatul <Edlinofuk@123>
2022-03-14 19:42:09 +0530

    2nd

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ calculation_to_units = 24
 name_of_unit = "hours"
 
 
-def days_to_units():
-    print(f" 20 days are {20 * calculation_to_units} {name_of_unit}")
+def days_to_units(num_of_days, custom_message):
+    print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
+    print(custom_message)
+
+def scope_check(num_of_days):
+    my_var = "variable inside function"
+    print(name_of_unit)
+    print(num_of_days)
+    print(my_var)
