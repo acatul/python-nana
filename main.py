281ed429721699d7ada63631c62359cc07065711
acatul <Edlinofuk@123>
2022-03-14 20:14:50 +0530

    3rd

--- main.py
+++ main.py
@@ -6,8 +6,4 @@ def days_to_units(num_of_days, custom_message):
     print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
     print(custom_message)
 
-def scope_check(num_of_days):
-    my_var = "variable inside function"
-    print(name_of_unit)
-    print(num_of_days)
-    print(my_var)
+input()
